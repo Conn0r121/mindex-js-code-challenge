630199bb88bd9814da443a71cf9968ccbdbead1c
Connor Robinson <connorrobinson121@gmai.com>
2021-12-17 16:11:33 -0500

    Task 2.2 - adding direct / indirect report list, count to html, and test

--- src/app/employee/employee.component.spec.ts
+++ src/app/employee/employee.component.spec.ts
@@ -2,7 +2,9 @@ import {async, TestBed} from '@angular/core/testing';
 import {Component} from '@angular/core';
 
 import {EmployeeComponent} from './employee.component';
-import { MatCardModule } from '@angular/material/card';
+import {MatCardModule} from '@angular/material/card';
+import {EmployeeService} from '../employee.service';
+import {of} from 'rxjs';
 
 @Component({selector: 'app-mat-card', template: ''})
 class CardComponent {
@@ -24,7 +26,51 @@ class CardSubtitleComponent {
 class CardContentComponent {
 }
 
+// adding suite of example employees for testing. reporting hierarchy is as follows
+//                  1
+//                /    \
+//               2      4
+//                       \
+//                        3
+const employee1 = {
+  id: 1,
+  firstName: 'first1',
+  lastName: 'last1',
+  position: 'jobTitle1',
+  compensation: 100,
+  directReports: [2, 4]
+};
+
+const employee2 = {
+  id: 2,
+  firstName: 'first2',
+  lastName: 'last2',
+  position: 'jobTitle2',
+  compensation: 200
+};
+
+const employee3 = {
+  id: 3,
+  firstName: 'first3',
+  lastName: 'last3',
+  position: 'jobTitle3',
+  compensation: 300
+};
+
+const employee4 = {
+  id: 4,
+  firstName: 'first4',
+  lastName: 'last4',
+  position: 'jobTitle4',
+  compensation: 400,
+  directReports: [3]
+};
+
+const employees = [employee1, employee2, employee3, employee4];
+
 const employeeServiceSpy = jasmine.createSpyObj('EmployeeService', ['getAll', 'get', 'save', 'remove']);
+
+employeeServiceSpy.get.and.callFake((employeeId: Number) => of(employees.find(employee => employee.id === employeeId)));
 
 describe('EmployeeComponent', () => {
   beforeEach(async(() => {
@@ -41,19 +87,45 @@ describe('EmployeeComponent', () => {
       imports: [
         MatCardModule,
       ],
+      providers: [
+        {provide: EmployeeService, useValue: employeeServiceSpy}
+      ]
     }).compileComponents();
   }));
 
   it('should create the component', async(() => {
     const fixture = TestBed.createComponent(EmployeeComponent);
     const comp = fixture.debugElement.componentInstance;
-    comp.employee = {
-      id: 1,
-      firstName: 'first',
-      lastName: 'last',
-      position: 'jobTitle'
-    };
+    comp.employee = employee1;
 
     expect(comp).toBeTruthy();
+  }));
+
+
+  it('should generate full report list on init with a single direct report', async(() => {
+    // testing to correctly ensure that employee3 reports to employee4
+    const fixture = TestBed.createComponent(EmployeeComponent);
+    const comp = fixture.debugElement.componentInstance;
+    comp.employee = employee4;
+    fixture.detectChanges();
+    expect(comp.reports).toEqual([employee3]);
+  }));
+
+  it('should generate full report list on init with a direct and indirect reports', async(() => {
+    // testing to correctly ensure that employee2, employee4, and employee3 report to employee1
+    const fixture = TestBed.createComponent(EmployeeComponent);
+    const comp = fixture.debugElement.componentInstance;
+    comp.employee = employee1;
+    fixture.detectChanges();
+    expect(comp.reports).toEqual([employee2, employee4, employee3]);
+  }));
+
+  it('should generate full report list on init with no reports', async(() => {
+  // testing to correctly ensure that no one reports to employee3
+    const fixture = TestBed.createComponent(EmployeeComponent);
+    const comp = fixture.debugElement.componentInstance;
+    comp.employee = employee3;
+    fixture.detectChanges();
+    expect(comp.reports).toEqual([]);
   }));
 });
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,15 +1,30 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 import {Employee} from '../employee';
+import {EmployeeService} from '../employee.service';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
   @Input() employee: Employee;
+  reports: Employee[] = [];
 
-  constructor() {
+  constructor(private employeeService: EmployeeService) {
+  }
+
+  ngOnInit(): void {
+    this.getAllReports(this.employee.directReports);
+  }
+
+  private getAllReports(employeeIds: number[] | null) {
+    employeeIds?.forEach(employeeId => {
+      this.employeeService.get(employeeId).subscribe(returnedEmployee => {
+        this.reports.push(returnedEmployee);
+        this.getAllReports(returnedEmployee.directReports);
+      });
+    });
   }
 }
